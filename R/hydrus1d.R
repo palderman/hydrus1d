db05f6e914c607a1583684494a1b1b96db6b1cb6
Phillip D. Alderman <phillip.alderman@okstate.edu>
2021-07-07 14:11:05 -0500

    Initial commit for R package linking Hydrus 1D code

---
+++ R/hydrus1d.R
@@ -0,0 +1,11 @@
+#' Hydrus 1-D
+#'
+#' A function to call the Hydrus 1-D model from R
+#'
+#' @examples
+#'
+#' hydrus1d()
+#'
+hydrus1d <- function() {
+  print("Call Hydrus 1-D here, then return results.")
+}
