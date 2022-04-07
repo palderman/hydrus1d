d64c25e28bea8982a272e11ba73b87e56b821ba4
Phillip D. Alderman <phillip.alderman@okstate.edu>
2022-04-07 15:36:08 -0500

    Adding code to adjust working directory path when running on Windows

--- R/hydrus1d.R
+++ R/hydrus1d.R
@@ -20,8 +20,15 @@ hydrus1d <- function(hydrus_wd) {
   curr_wd <- getwd()
   on.exit(setwd(curr_wd))
 
+  level_01.dir <- file.path(hydrus_wd, "LEVEL_01.DIR")
+
   # write the hydrus working directory to LEVEL_01.DIR
-  write(hydrus_wd, file.path(hydrus_wd, "LEVEL_01.DIR"))
+  if(Sys.info()["sysname"] == "Windows"){
+    hydrus_wd <- str_replace_all(hydrus_wd, c("/"="\\\\"))
+    level_01.dir <- str_replace_all(level_01.dir, c("/"="\\\\"))
+  }
+
+  write(hydrus_wd, level_01.dir)
 
   # set working directory to the hydrus working directory
   setwd(hydrus_wd)
